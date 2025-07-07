7b7152fb8e39f8fcbc6793d1a6028f965ff392ae
elizabethhoyt <bella.andre@tuta.io>
2025-07-07 14:36:48 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Lifelock Norrton'
+project = 'McAfee Safe'
 copyright = '2025'
-author = 'Woodrow Landfair'
+author = 'Elizabeth Hoyt'
 
 release = '1.0'
 
